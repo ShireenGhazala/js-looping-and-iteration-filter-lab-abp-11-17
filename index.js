5818028b4acc1f6cde5c525b8496409e4df1b519
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-15 08:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-drivers.filter(findMatching(name){
-let driver=driver.toUpperCase();
-let name=name.toUpperCase();
-if (driver===name){
-  return true};
-});
+// drivers.filter(findMatching(name){
+// let driver=driver.toUpperCase();
+// let name=name.toUpperCase();
+// if (driver===name){
+  // return true};
+// });
